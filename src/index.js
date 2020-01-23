7fb5bc3cfff9720b81cb5d07065b0f8d42f4f484
Flavio Vinicius Miranda <vinniimiranda@gmail.com>
2020-01-23 11:23:13 -0300

    dotenv added

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap-grid.min.css";
 import "react-toastify/dist/ReactToastify.min.css";
+import "dotenv/config";
 import App from "./App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
